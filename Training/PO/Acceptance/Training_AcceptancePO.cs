aea130f9efb74fb1e576b05fd83743f481de6186
xuanvinh223 <phamxuanvinh020203@gmail.com>
2024-08-09 13:14:50 +0700

    add folder Training

---
+++ Training/PO/Acceptance/Training_AcceptancePO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+
+namespace Training.Acceptance.PO
+{
+    internal class Training_AcceptancePO :Ede.Uof.Utility.Data.BasePersistentObject
+    {
+        internal DataTable GetData(string RKNO)
+        {
+
+            string connStr = Training.Properties.Settings.Default.LYV_ERP.ToString();
+            this.m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connStr);
+            string cmdTxt = @"SELECT ROW_NUMBER() OVER(ORDER BY KCRKS.CLBH) AS Seq, KCRKS.CLBH, CLZL.YWPM, CLZL.DWBH,
+                                CONVERT(float, KCRKS.Qty)as Qty, CONVERT(float,isnull(KCRKS.USPrice, KCRKS.VNPrice ) ) as USPrice, CONVERT(float,isnull(KCRKS.USACC, KCRKS.VNACC )) USACC , KCRK.ZSNO, ZSZL.ZSYWJC, KCRKS.RKNO FROM KCRKS 
+                                LEFT JOIN KCRK ON KCRK.RKNO = KCRKS.RKNO
+                                LEFT JOIN CLZL ON CLZL.CLDH = KCRKS.CLBH 
+                                LEFT JOIN ZSZL ON KCRK.ZSBH = ZSZL.ZSDH
+                                WHERE KCRKS.RKNO = @RKNO";
+
+            this.m_db.AddParameter("@RKNO", RKNO);
+
+            DataTable dt = new DataTable();
+
+            dt.Load(this.m_db.ExecuteReader(cmdTxt));
+            return dt;
+
+        }
+    }
+}
---
+++ Training/UCO/Acceptance/AcceptanceUCO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Training.Acceptance.PO;
+using Training.Data;
+using System.Xml.Linq;
+
+namespace Training.UCO.Acceptance
+{
+    public class AcceptanceUCO
+    {
+        Training_AcceptancePO AcceptancePO = new Training_AcceptancePO();
+
+
+        public DataTable GetData(string RKNO)
+        {
+            return AcceptancePO.GetData(RKNO);
+        }
+    }
+}
